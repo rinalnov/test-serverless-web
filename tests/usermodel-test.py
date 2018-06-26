f2ac1e8e1821772c0bdb8c5796ee4016dd5625d1
RinalNov <>
2018-06-26 11:24:55 +0700

    add test code coverage & assertion

--- tests/usermodel-test.py
+++ tests/usermodel-test.py
@@ -4,8 +4,10 @@ from src.usermodel import Usermodel
 class UsermodelTest(unittest.TestCase):
     def test_fail_addition(self):
         # Make test fail
-        self.assertEqual(Usermodel.addition(3, 4), 8)
+        self.assertNotEqual(Usermodel.addition(3, 4), 8)
     
     def test_correct_addition(self):
         # Make test fail
         self.assertEqual(Usermodel.addition(5, 10), 15)
+
+    
